c1e6fbcbe007c53f326507bc6f6564f106713fe6
Sanelisiwe Myburg <sane.myburg@gmail.com>
2022-10-24 21:53:46 +0200

    Dynamically add artists info

---
+++ script.js
@@ -0,0 +1,54 @@
+const artists = [
+  {
+    artistImage: './images/AKA_Supa_Mega.png',
+    artistName: 'Aka Supa Mega',
+    artistGenre: 'South African Hip Hop Artist',
+    artistInfo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    artistImage: './images/ami-faku.png',
+    artistName: 'Ami Faku',
+    artistGenre: 'South African R&B and soul Artist',
+    artistInfo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    artistImage: './images/dj-zinhle.png',
+    artistName: 'Dj Zinhle',
+    artistGenre: 'South African Female dj',
+    artistInfo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    artistImage: './images/loyiso.png',
+    artistName: 'Loyiso',
+    artistGenre: 'South African R&B Artist',
+    artistInfo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    artistImage: './images/NASTY-C-COLOUR-2.png',
+    artistName: 'Nasty C',
+    artistGenre: 'South African Hip Hop Artist',
+    artistInfo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    artistImage: './images/zahara.png',
+    artistName: 'Zahara',
+    artistGenre: 'South African Afro Soul Artist',
+    artistInfo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+];
+
+const featureArtist = document.querySelector('.artists-list');
+artists.forEach((e) => {
+  const div = document.createElement('div');
+  div.innerHTML += `
+    <div class="artist-card">
+    <img src="${e.artistImage}">
+    <div>
+       <h3>${e.artistImage}</h3>
+       <h5>${e.artistGenre}</h5>
+       <p>${e.artistInfo}</p>
+    </div>
+    </div>
+    `;
+  featureArtist.append(div);
+});
